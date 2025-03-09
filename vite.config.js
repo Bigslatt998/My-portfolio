5c38bdea600c480038e249fed2702ac4f19772ca
BigSlatt998 <ganiyutaofeeqolarewaju@gmail.com>
2025-03-09 11:40:19 +0100

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/icode/',
   server: {
     host: true,
-  }
+  },
+  base: "/icodexx8"
 })
